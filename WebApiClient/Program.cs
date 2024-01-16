aff403e95cb0c2d78efa054333835ae8be3c0038
ghaith allah ben hassine <ghaithallahbenhassine666@gmail.com>
2024-01-16 18:21:48 +0100

    first commit

---
+++ WebApiClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace WebApiClient
+{
+    class Program
+    {
+        static async Task Main(string[]args)
+        {
+            Console.WriteLine("press any key...");
+            Console.ReadLine();
+
+            using (System.Net.Http.HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("http://localhost:5109/Values");
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    string message = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(message);
+                }
+                else
+                {
+                    Console.WriteLine($"response error code:{response.StatusCode}");
+                }
+            }
+
+
+            Console.ReadLine();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+
---
+++ WebApiServer/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public String[] Get ()
+        {
+            return new String[] { "mango", "banana","pear","apple" };
+        }
+    }
+
+}
